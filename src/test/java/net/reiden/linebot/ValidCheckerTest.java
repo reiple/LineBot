8b92de67c45ccd1b29b92ec38f369f1365f97be7
이준훈 <trsprs@gmail.com>
2022-03-20 13:32:16 +0900

    테스트 클래스의 public 제거

--- src/test/java/net/reiden/linebot/ValidCheckerTest.java
+++ src/test/java/net/reiden/linebot/ValidCheckerTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ValidCheckerTest {
+class ValidCheckerTest {
 
     private ValidChecker validChecker;
 
